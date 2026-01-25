44417f4cef2b88e4302f9b352e244d6d6eed46a3
NoobOtaku <dhakad.dj9580@gmail.com>
2026-01-25 17:17:33 +0530

    Fix frontend build errors: import syntax and CSS order
    - Fix useAuthStore import: change from default to named export
    - Fix CSS @import order: move Google Fonts import before @tailwind directives

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ChefHat, Trophy, Star, Users } from 'lucide-react'
-import useAuthStore from '../store/authStore'
+import { useAuthStore } from '../store/authStore'
 
 export default function Home() {
   const { user } = useAuthStore()
